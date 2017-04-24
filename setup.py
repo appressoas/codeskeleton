4197503c98939cdb3c376243508112d87da04dfe
Espen A. Kristiansen <post@espenak.net>
2017-04-24 19:01:35 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description='General purpose code skeleton solution.',
     version='1.0',
     author='Appresso AS',
+    author_email='post@appresso.no',
     packages=find_packages(),
     install_requires=[
         'PyYAML',
@@ -16,7 +17,17 @@ setup(
     ],
     include_package_data=True,
     zip_safe=False,
+    license='BSD',
     entry_points={
         "console_scripts": ['codeskeleton = codeskeleton.cli.codeskeleton_cli:main']
-    }
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Code Generators',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved',
+        'Programming Language :: Python :: 3'
+    ],
 )
